50624e9598c13f5311c4bc7c7a5029ec91852c07
RohiniMane007 <manerohini295@gmail.com>
2025-04-02 17:24:03 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { Provider } from 'react-redux'
-// import store from './store/store.js'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-
-//   </StrictMode>,
-// )
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
